a85b8674465a889ae97f7fde7c8f1f0fc7669461
Zyon <zyonamadeus01@gmail.com>
2019-05-29 00:13:15 -0300

    Update Leitura.java

--- Leitura.java
+++ Leitura.java
@@ -1,28 +1,32 @@
-package exercicio;
+package Poo1;
 
 import java.io.*;
 
-public class Leitura{
-	public static String lerString(String label) {
-		InputStreamReader input = new InputStreamReader(System.in);
-		BufferedReader ler = new BufferedReader(input);
-		String X = "";
-		System.out.print(label);
-		try{
-			X = ler.readLine();			
-		} catch(IOException erro) {
-			System.out.println("Campo inválido!!");
-		}		
-		return X;			
-	}    
-		
-	public static String lerString(){
-		return lerString("");
-	}
-	public static int lerInt() {
-		return Integer.parseInt(lerString());		
-	}
-	public static double lerDouble(){
-		return Double.parseDouble(lerString());
-	}
+public class Leitura {
+    
+    //metodos
+    public static String lerString(String label) {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader ler = new BufferedReader(input);
+        String X = "";
+        System.out.print(label);
+        try {
+            X = ler.readLine();
+        } catch (IOException erro) {
+            System.out.println("Campo invalido!!");
+        }
+        return X;
+    }
+
+    public static String lerString() {
+        return lerString("");
+    }
+
+    public static int lerInt() {
+        return Integer.parseInt(lerString());
+    }
+
+    public static double lerDouble() {
+        return Double.parseDouble(lerString());
+    }
 }
