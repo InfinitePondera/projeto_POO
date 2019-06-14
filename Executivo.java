33d42b6e54831bb7a543d0836b3154524bf052f7
Zyon <zyonamadeus01@gmail.com>
2019-06-13 22:40:07 -0300

    Update Executivo.java

--- Executivo.java
+++ Executivo.java
@@ -3,7 +3,14 @@ package projetofinal;
 public class Executivo extends Automovel
 {
     private double vlOpcionais;
-
+    //
+    public double getVlOpcionais(){
+        return this.vlOpcionais;
+    }
+    public void setVlOpcionais(double vlOpcionais){
+        this.vlOpcionais = vlOpcionais;
+    }
+    //
     public Executivo(String placa, String cor, int nroPortas, char combtipo, long km, double valorDiaria, double vlopc)
     {
         super(placa, cor, nroPortas, combtipo, km, valorDiaria);
