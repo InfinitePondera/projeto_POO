ecdba57873b75b4e4613c0ad14f7947f9e674737
JoooooooooJ <50976403+JoooooooooJ@users.noreply.github.com>
2019-06-13 22:17:13 -0300

    Update Onibus.java

--- Onibus.java
+++ Onibus.java
@@ -8,8 +8,17 @@ public class Onibus extends Veiculo
 
     //
 
-    public static Onibus criar()
+    public Onibus(String placa, String cor, int nroPortas, char combtipo, long km, double valorDiaria, int npassag, int vlseguro, boolean guia)
     {
-        Onibus onibus = new Onibus();
+        super(placa, cor, nroPortas, combtipo, km, valorDiaria);
+        this.nroPassageiros = npassag;
+        this.vlSeguroPassageiro = vlseguro;
+        this.guia = guia;
+    }
+
+    public static Onibus criar(String placa, String cor, int nroPortas, char combtipo, long km, double valorDiaria, int npassag, int vlseguro, boolean guia)
+    {
+        Onibus onibus = new Onibus(placa, cor, nroPortas, combtipo, km, valorDiaria, npassag, vlseguro, guia);
+        return onibus;
     }
 }
