3e7f0948eb991fd8349a2076ba80adcffedd0402
CesarO99 <50976490+CesarO99@users.noreply.github.com>
2019-06-13 22:38:39 -0300

    Update Locacao.java

--- Locacao.java
+++ Locacao.java
@@ -15,9 +15,21 @@ public class Locacao
     private double valorLocacao;
     private boolean paga; 
 
-    //construtores
-
-    //gets-sets
+    //construtor
+    public Locacao(int locId, Cliente client, Veiculo veic, Calendar dtsaida, Calendar dtvoltaprev, Calendar dtvolta, Long kmsaida, Long kmvolta, double vlcaucao, double vlloc, boolean pg)
+    {
+        locacaoID = locId;
+        cliente = client;
+        veiculo = veic;
+        dtLocacao = dtsaida; 
+        dtDevolucaoPrevista = dtvoltaprev;
+        dtDevolucao = dtvolta;
+        kmLocacao = kmsaida;
+        kmDevolucao = kmvolta;
+        valorCaucao = vlcaucao;
+        valorLocacao = vlloc;
+        paga = pg;
+    }
 
     //metodos
 
@@ -29,16 +41,18 @@ public class Locacao
 
     public static void listar(Locacao[] listLoc)
     {
-        
+        system.out.println("locado: ", listLoc[]);
     }
     public double calculaLocacao()
     {
-        
+        return veiculo.veicalculaCustos();
     }
     public boolean pagar()
     {
 
     }
+
+    //gets-sets
 
 
 
