f452116fae3cfba19cce229dd2031e6d41f9d570
JoooooooooJ <50976403+JoooooooooJ@users.noreply.github.com>
2019-06-13 22:17:32 -0300

    Update Executivo.java

--- Executivo.java
+++ Executivo.java
@@ -4,8 +4,15 @@ public class Executivo extends Automovel
 {
     private double vlOpcionais;
 
-    public static Executivo criar()
+    public Executivo(String placa, String cor, int nroPortas, char combtipo, long km, double valorDiaria, double vlopc)
     {
-        Executivo exec = new Executivo();
+        super(placa, cor, nroPortas, combtipo, km, valorDiaria);
+        this.vlOpcionais = vlopc;   
+    }
+
+    public static Executivo criar(String placa, String cor, int nroPortas, char combtipo, long km, double valorDiaria, double vlopc)
+    {
+        Executivo exec = new Executivo(placa, cor, nroPortas, combtipo, km, valorDiaria,vlpopc);
+        return exec;
     }
 }
