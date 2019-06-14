819a8e9a64a1fd262007a91c0b6a856f5fcc4826
JoooooooooJ <50976403+JoooooooooJ@users.noreply.github.com>
2019-06-13 22:38:37 -0300

    Update Veiculo.java

--- Veiculo.java
+++ Veiculo.java
@@ -14,21 +14,35 @@ public class Veiculo
 
     //construtores
 
-      public Veiculo(String placa, String cor, int nportas, char combustivel, long kmrodados, double valordiaria)
-      {
-          this.placa = placa;
-          this.cor = cor;
-          this.nroPortas = nportas;
-          this.tipoCombustivel = combustivel;
-          this.quilometragem = kmrodados;
-          this.valorDiaria = valordiaria;
-      }
+    public Veiculo(String placa, String cor, int nportas, char combustivel, long kmrodados, double valordiaria)
+    {
+        this.placa = placa;
+        this.cor = cor;
+        this.nroPortas = nportas;
+        this.tipoCombustivel = combustivel;
+        this.quilometragem = kmrodados;
+        this.valorDiaria = valordiaria;
+    }
 
     //metodos
 
     public double calculaCustos(int dias, long km)
     {
+        double custo;
+        int diferenca;
 
+        diferenca = km - (dias * 100);         
+        custo = valorDiaria * dias;
+
+        if(diferenca>0)
+        {
+            for(int i = 0;i<diferenca;i++)
+            {
+                custo += Utilitaria.custoKmExtra; 
+            }
+        }
+
+        return custo;
     }
 
 }
