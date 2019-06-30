7cb815f284661ec9f71ec40ee3ec01c5fd10c15f
Zyon <zyonamadeus01@gmail.com>
2019-06-30 19:42:07 -0300

    Update Onibus.java

--- Onibus.java
+++ Onibus.java
@@ -1,45 +1,58 @@
-    
+
 package projetofinal;
 
-public class Onibus extends Veiculo
-{
+public class Onibus extends Veiculo {
     private int nroPassageiros;
     private double vlSeguroPassageiro;
     private boolean guia;
 
     //
 
-    public Onibus(String placa, String cor, int nroPortas, char combtipo, long km, double valorDiaria, int npassag, int vlseguro, boolean guia)
-    {
+    public Onibus(String placa, String cor, int nroPortas, char combtipo, long km, double valorDiaria, int npassag,
+            int vlseguro, boolean guia) {
         super(placa, cor, nroPortas, combtipo, km, valorDiaria);
         this.nroPassageiros = npassag;
         this.vlSeguroPassageiro = vlseguro;
         this.guia = guia;
     }
+
     //
-    public int getNroPassageiros(){
+    public int getNroPassageiros() {
         return this.nroPassageiros;
     }
-    public void setNroPassageiros(int nroPassageiros){
+
+    public void setNroPassageiros(int nroPassageiros) {
         this.nroPassageiros = nroPassageiros;
     }
-    public double getVlSeguroPassageiro(){
+
+    public double getVlSeguroPassageiro() {
         return this.vlSeguroPassageiro;
     }
-    public void setVlSeguroPassageiro(double vlSeguroPassageiro){
+
+    public void setVlSeguroPassageiro(double vlSeguroPassageiro) {
         this.vlSeguroPassageiro = vlSeguroPassageiro;
     }
-    public boolean getGuia(){
+
+    public boolean getGuia() {
         return this.guia;
     }
-    public void setGuia(boolean guia){
+
+    public void setGuia(boolean guia) {
         this.guia = guia;
     }
+
     //
-    public static Onibus criar()
-    {
-        String placa, String cor, int nroPortas, char combtipo, long km, double valorDiaria, int npassag, int vlseguro, boolean guia;
-        
+    public static Onibus criar() {
+        String placa;
+        String cor;
+        int nroPortas;
+        char combtipo;
+        long km;
+        double valorDiaria;
+        int npassag;
+        int vlseguro;
+        boolean guia;
+
         Scanner entrada = new Scanner(System.in);
         placa = leitura.lerString;
         cor = leitura.lerString;
@@ -54,19 +67,17 @@ public class Onibus extends Veiculo
         Onibus onibus = new Onibus(placa, cor, nroPortas, combtipo, km, valorDiaria, npassag, vlseguro, guia);
         return onibus;
     }
-    public double calculaCustos(int dias, long km)
-    {
+
+    public double calculaCustos(int dias, long km) {
         double custo;
         int diferenca;
 
-        diferenca = km - (dias * 100);         
-        custo = valorDiaria * dias;
+        diferenca = km - (dias * 100);
+        custo = super.getValorDiaria() * dias;
 
-        if(diferenca>0)
-        {
-            for(int i = 0;i<diferenca;i++)
-            {
-                custo += Utilitaria.custoKmExtra; 
+        if (diferenca > 0) {
+            for (int i = 0; i < diferenca; i++) {
+                custo += Utilitaria.custoKmExtra;
             }
         }
 
@@ -75,5 +86,10 @@ public class Onibus extends Veiculo
 
         return custo;
     }
-}
 
+    public String toString() {
+        return super.getVeiculoID() + "|" + super.getPlaca() + "|" + super.getCor() + "|" + super.getNroPortas() + "|"
+                + super.getTipoCombustivel + "|" + super.getQuilometragem + "|" + super.getValorDiaria + "|"
+                + this.nroPassageiros + "|" + this.vlSeguroPassageiro + "|" + this.guia;
+    }
+}
