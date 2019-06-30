6766ca7da9e05b1cd63697d38f1f2184e665495f
Zyon <zyonamadeus01@gmail.com>
2019-06-30 19:42:45 -0300

    Update Veiculo.java

--- Veiculo.java
+++ Veiculo.java
@@ -1,8 +1,7 @@
 package projetofinal;
 
-public class Veiculo
-{
-    //atributos
+public class Veiculo {
+    // atributos
 
     private int veiculoID;
     private String placa;
@@ -12,12 +11,8 @@ public class Veiculo
     private long quilometragem;
     private double valorDiaria;
 
-    //construtores
-
-    public Veiculo(String placa, String cor, int nportas, char combustivel, long kmrodados, double valordiaria)
-    {
-        this.veiculoID = Utilitaria.idVeiculo;
-        Utilitaria.idVeiculo++;
+    // construtores
+    public Veiculo(String placa, String cor, int nportas, char combustivel, long kmrodados, double valordiaria) {
         this.placa = placa;
         this.cor = cor;
         this.nroPortas = nportas;
@@ -25,68 +20,84 @@ public class Veiculo
         this.quilometragem = kmrodados;
         this.valorDiaria = valordiaria;
     }
-    //get set
-    public int getVeiculoID(){
+
+    // get set
+    public int getVeiculoID() {
         return this.veiculoID;
     }
-    public void setVeiculoID(int veiculoID){
+
+    public void setVeiculoID(int veiculoID) {
         this.veiculoID = veiculoID;
     }
-    public String getPlaca(){
+
+    public String getPlaca() {
         return this.placa;
     }
-    public void setPlaca(String placa){
+
+    public void setPlaca(String placa) {
         this.placa = placa;
     }
-    public String getCor(){
+
+    public String getCor() {
         return this.cor;
     }
-    public void setCor(String cor){
+
+    public void setCor(String cor) {
         this.cor = cor;
     }
-    public int getNroPortas(){
+
+    public int getNroPortas() {
         return this.nroPortas;
     }
-    public void setNroPortas(int nroPortas){
+
+    public void setNroPortas(int nroPortas) {
         this.nroPortas = nroPortas;
     }
-    public char getTipoCombustivel(){
+
+    public char getTipoCombustivel() {
         return this.tipoCombustivel;
     }
-    public void setTipoCombustivel(char tipoCombustivel){
+
+    public void setTipoCombustivel(char tipoCombustivel) {
         this.tipoCombustivel = tipoCombustivel;
     }
-    public long getQuilometragem(){
+
+    public long getQuilometragem() {
         return this.quilometragem;
     }
-    public void setQuilometragem(long quilometragem){
+
+    public void setQuilometragem(long quilometragem) {
         this.quilometragem = quilometragem;
     }
-    public double getValorDiaria(){
+
+    public double getValorDiaria() {
         return this.valorDiaria;
     }
-    public void setValorDiaria(double valordiaria){
+
+    public void setValorDiaria(double valordiaria) {
         this.valorDiaria = valordiaria;
     }
-    //metodos
 
-    public double calculaCustos(int dias, long km)
-    {
+    // metodos
+    public double calculaCustos(int dias, long km) {
         double custo;
         int diferenca;
 
-        diferenca = km - (dias * 100);         
+        diferenca = km - (dias * 100);
         custo = valorDiaria * dias;
 
-        if(diferenca>0)
-        {
-            for(int i = 0;i<diferenca;i++)
-            {
-                custo += Utilitaria.custoKmExtra; 
+        if (diferenca > 0) {
+            for (int i = 0; i < diferenca; i++) {
+                custo += Utilitaria.custoKmExtra;
             }
         }
 
         return custo;
+    }
+
+    public String toString() {
+        return this.veiculoID + "|" + this.placa + "|" + this.cor + "|" + this.nroPortas + "|" + this.tipoCombustivel
+                + "|" + this.quilometragem + "|" + this.valorDiaria;
     }
 
 }
