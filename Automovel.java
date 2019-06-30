f1b2bf3d7f7cd3bb370ee4ea4af3a06ef3d8cb7c
Zyon <zyonamadeus01@gmail.com>
2019-06-30 19:38:35 -0300

    Update Automovel.java

--- Automovel.java
+++ Automovel.java
@@ -1,17 +1,36 @@
 package projetofinal;
 
-public class Automovel extends Veiculo
-{
+import java.util.Scanner;
+
+public class Automovel extends Veiculo {
     //
 
-    public Automovel(String placa, String cor, int nroPortas, char combtipo, long km, double valorDiaria)
-    {
+    public Automovel(String placa, String cor, int nroPortas, char combtipo, long km, double valorDiaria) {
         super(placa, cor, nroPortas, combtipo, km, valorDiaria);
     }
 
-    public static Automovel criar(String placa, String cor, int nroPortas, char combtipo, long km, double valorDiaria)
-    {
+    public static Automovel criar() {
+        Scanner entrada = new Scanner(System.in);
+        String placa;
+        String cor;
+        int nroPortas;
+        char combtipo;
+        long km;
+        double valorDiaria;
+
+        placa = leitura.lerString;
+        cor = leitura.lerString;
+        nroPortas = leitura.lerInt;
+        combtipo = entrada.next().charAt(0);
+        km = entrada.nextLong();
+        valorDiaria = leitura.lerDouble;
+
         Automovel auto = new Automovel(placa, cor, nroPortas, combtipo, km, valorDiaria);
         return auto;
+    }
+
+    public String toString() {
+        return super.getVeiculoID() + "|" + super.getPlaca() + "|" + super.getCor() + "|" + super.getNroPortas() + "|"
+                + super.getTipoCombustivel + "|" + super.getQuilometragem + "|" + super.getValorDiaria;
     }
 }
