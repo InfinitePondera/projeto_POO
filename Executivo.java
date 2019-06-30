ef3e662ba43bcf53b35bc12aaec12b9d13cc4168
Zyon <zyonamadeus01@gmail.com>
2019-06-30 19:41:09 -0300

    Update Executivo.java

--- Executivo.java
+++ Executivo.java
@@ -1,50 +1,57 @@
 package projetofinal;
 
-public class Executivo extends Automovel
-{
+public class Executivo extends Automovel {
+
     private double vlOpcionais;
+
     //
-    public double getVlOpcionais(){
+    public double getVlOpcionais() {
         return this.vlOpcionais;
     }
-    public void setVlOpcionais(double vlOpcionais){
+
+    public void setVlOpcionais(double vlOpcionais) {
         this.vlOpcionais = vlOpcionais;
     }
+
     //
-    public Executivo(String placa, String cor, int nroPortas, char combtipo, long km, double valorDiaria, double vlopc)
-    {
+    public Executivo(String placa, String cor, int nroPortas, char combtipo, long km, double valorDiaria,
+            double vlopc) {
         super(placa, cor, nroPortas, combtipo, km, valorDiaria);
-        this.vlOpcionais = vlopc;   
+        this.vlOpcionais = vlopc;
     }
 
-    public static Executivo criar()
-    {
+    public static Executivo criar() {
         String placa;
-        String cor; 
+        String cor;
         int nroPortas;
-        char combtipo; 
-        long km; 
-        double valorDiaria; 
+        char combtipo;
+        long km;
+        double valorDiaria;
         double vlopc;
-        
+
         Scanner entrada = new Scanner(System.in);
         placa = leitura.lerString;
         cor = leitura.lerString;
         nroPortas = leitura.lerInt;
         combtipo = entrada.next().charAt(0);
-        long = entrada.nextLong();
+        km = entrada.nextLong();
         valorDiaria = leitura.lerDouble;
         vlopc = leitura.lerDouble;
-        Executivo exec = new Executivo(placa, cor, nroPortas, combtipo, km, valorDiaria,vlpopc);
+        Executivo exec = new Executivo(placa, cor, nroPortas, combtipo, km, valorDiaria, vlpopc);
         return exec;
     }
 
-    public double calculaCustos(int dias, long km)
-    {
+    public double calculaCustos(int dias, long km) {
         double custo;
 
-        custo = (valorDiaria * dias) + vlOpcionais;
+        custo = (super.getValorDiaria() * dias) + vlOpcionais;
 
         return custo;
+    }
+
+    public String toString() {
+        return super.getVeiculoID() + "|" + super.getPlaca() + "|" + super.getCor() + "|" + super.getNroPortas() + "|"
+                + super.getTipoCombustivel + "|" + super.getQuilometragem + "|" + super.getValorDiaria + "|"
+                + this.vlOpcionais;
     }
 }
