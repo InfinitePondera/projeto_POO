d54f6e99465b7fbfa73b75bbd00a43ba606479d7
Zyon <zyonamadeus01@gmail.com>
2019-06-30 22:07:57 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,36 +1,38 @@
 package projetofinal;
 
-public class Main
-
-{    public static void main(String[] args)
-    {
+public class Main{
+	public static void main(String[] args) {
+		int menu;
+		do {
         System.out.println("----== LOCADORA -DE- AUTOMOVEIS ==----");
-        int menu, Ctipo, Vtipo;
+        int Ctipo, Vtipo;
         int Lcount;
-        Locacao[] loc;
+        int cont = 0;
+        Locacao[] loc = new Locacao[99];
 
         System.out.printf("\n1 -- Realizar uma nova Locação ------\n");
         System.out.printf("\n2 -- Realizar varias Locações -------\b");
         System.out.printf("\n3 -- Realizar cadastro de Cliente ---\n");
         System.out.printf("\n4 -- Realizar cadastro de Veiculos --\n");
         System.out.printf("\n5 -- Listar Locações ----------------\n");
-        menu = Leitura.lerString();
+        menu = Leitura.lerInt();
 
         switch (menu) {
             case 1:
-                Locacao.criar();                                            
+            	loc [cont] = Locacao.criar();
+            	cont++;
                 break;
 
             case 2:
 
-                System.out.println("Digite a quantidade de locações que deseja cadastrar");
-                Lcount = Leitura.lerInt(),
+                System.out.println("Digite a quantidade de locaﾃｧﾃｵes que deseja cadastrar");
+                Lcount = Leitura.lerInt();
                 
                 loc = new Locacao[Lcount];
 
                 for(int i = 0; i< Lcount;i++)
                 {
-                    loc[i] = Locacao.criar();
+                    loc[i].criar();
                 }
                 break;
 
@@ -55,10 +57,10 @@ public class Main
 		        System.out.printf("3 para Automovel\n");
                 System.out.printf("4 para Executivo\n");
                 Vtipo = Leitura.lerInt();
-                    switch(tipo)
+                    switch(Vtipo)
                     {
                         case 1:
-                            Uilitario.criar();
+                            Utilitario.criar();
                             break;
                         case 2:
                             Onibus.criar();
@@ -76,8 +78,7 @@ public class Main
                 Locacao.listar(loc);
                     
                 break;
-                    
-
         }
-    }
+	}while(menu != 0);
+	}
 }
