cbb3436a6de71106e01de6f1f8cb84387855fca7
Zyon <zyonamadeus01@gmail.com>
2019-07-01 00:30:33 -0300

    Update Automovel.java

--- Automovel.java
+++ Automovel.java
@@ -17,12 +17,18 @@ public class Automovel extends Veiculo {
         char combtipo;
         long km;
         double valorDiaria;
-
+        
+        System.out.println("\n Digite o numero da placa: ");
         placa = Leitura.lerString();
+        System.out.println("\n Digite a cor do carro: ");
         cor = Leitura.lerString();
+        System.out.println("\n Digite o numero de portas: ");
         nroPortas = Leitura.lerInt();
+        System.out.println("\n Digite o tipo do combustível(G, A, D): ");
         combtipo = entrada.next().charAt(0);
+        System.out.println("\n Digite a quilometragem: ");
         km = entrada.nextLong();
+        System.out.println("\n Digite o valor da diária: ");
         valorDiaria = Leitura.lerDouble();
 
         Automovel auto = new Automovel(placa, cor, nroPortas, combtipo, km, valorDiaria);
