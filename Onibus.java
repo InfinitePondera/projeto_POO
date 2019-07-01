417f3c25b1c0e430d55374fcde37eaae3b0e8e32
Zyon <zyonamadeus01@gmail.com>
2019-07-01 00:37:20 -0300

    Update Onibus.java

--- Onibus.java
+++ Onibus.java
@@ -56,13 +56,21 @@ public class Onibus extends Veiculo {
         boolean guia;
 
         Scanner entrada = new Scanner(System.in);
+        System.out.println("\n Digite o numero da placa: ");
         placa = Leitura.lerString();
+        System.out.println("\n Digite a cor do carro: ");
         cor = Leitura.lerString();
+        System.out.println("\n Digite o numero de portas: ");
         nroPortas = Leitura.lerInt();
+        System.out.println("\n Digite o tipo do combustível(G, A, ou D): ");
         combtipo = entrada.next().charAt(0);
+        System.out.println("\n Digite a quilometragem: ");
         km = entrada.nextLong();
+        System.out.println("\n Digite o valor da diária: ");
         valorDiaria = Leitura.lerDouble();
+        System.out.println("\n Digite o numero de passageiros: ");
         npassag = Leitura.lerInt();
+        System.out.println("\n Digite o valor do seguro: ");
         vlseguro = Leitura.lerInt();
         guia = true;
 
